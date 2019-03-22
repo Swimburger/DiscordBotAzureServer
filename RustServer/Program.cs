5731f73fd2a111a1cbed98e6bb5b331253b09972
Niels Swimberghe <nielsswimberghe@gmail.com>
2019-03-22 17:13:18 -0400

    Initial commit

---
+++ RustServer/Program.cs
@@ -0,0 +1,95 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace RustServer
+{
+    public class Program
+    {
+        private static DiscordClient discord;
+        private static VirtualMachineService virtualMachineService;
+        private static Random random = new Random();
+
+        public static async Task Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                 .AddEnvironmentVariables()
+                 .Build();
+
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = configuration["DiscordToken"],
+                TokenType = TokenType.Bot
+            });
+
+            virtualMachineService = new VirtualMachineService(configuration["VmResourceGroup"], configuration["VmName"]);
+            discord.MessageCreated += Discord_MessageCreated;
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task Discord_MessageCreated(MessageCreateEventArgs e)
+        {
+            try
+            {
+                if (e.Message.Content.ToLower().StartsWith("#!server start", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    IVirtualMachine vm = await virtualMachineService.GetVirtualMachineAsync();
+                    if (vm.PowerState == PowerState.Running)
+                    {
+                        await e.Message.RespondAsync("Rust server is already running! It takes a few minutes before rust server is connectable.");
+                        return;
+                    }
+
+                    if (vm.PowerState == PowerState.Starting)
+                    {
+                        await e.Message.RespondAsync("Rust server is already starting!");
+                        return;
+                    }
+
+                    Task startTask = virtualMachineService.StartVirtualMachineAsync(vm);
+                    await e.Message.RespondAsync("Starting Rust Server!");
+                    await startTask;
+                    await e.Message.RespondAsync("Rust Server started, ETA 5 min until you can connect! Hell yeah bizzes.");
+                    return;
+                }
+
+                if (e.Message.Content.StartsWith("#!server stop", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    IVirtualMachine vm = await virtualMachineService.GetVirtualMachineAsync();
+                    if (vm.PowerState == PowerState.Deallocated || vm.PowerState == PowerState.Deallocating || vm.PowerState == PowerState.Stopped)
+                    {
+                        await e.Message.RespondAsync("Rust server is stopped!");
+                        return;
+                    }
+
+                    if (vm.PowerState == PowerState.Stopping)
+                    {
+                        await e.Message.RespondAsync("Rust server is already stopping!");
+                        return;
+                    }
+
+                    Task stopTask = virtualMachineService.StopVirtualMachineAsync(vm);
+                    await e.Message.RespondAsync("Stopping Rust Server!");
+                    await stopTask;
+                    await e.Message.RespondAsync("Rust Server stopped, goodnight :(");
+                    return;
+                }
+
+                if (e.Message.Content.StartsWith("#!ping", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await e.Message.RespondAsync("Pong");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                await e.Message.RespondAsync(ex.Message);
+            }
+        }
+    }
+}
---
+++ RustServer/VirtualMachineService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System.Threading.Tasks;
+
+namespace RustServer
+{
+    public class VirtualMachineService
+    {
+        private readonly string resourceGroup;
+        private readonly string vmName;
+
+        public VirtualMachineService(string resourceGroup, string vmName)
+        {
+            this.resourceGroup = resourceGroup;
+            this.vmName = vmName;
+        }
+
+        public async Task<IVirtualMachine> GetVirtualMachineAsync()
+        {
+            AzureCredentials credentials = SdkContext.AzureCredentialsFactory.FromMSI(new MSILoginInformation(MSIResourceType.AppService), AzureEnvironment.AzureGlobalCloud);
+
+            IAzure azure = Azure
+                .Configure()
+                .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                .Authenticate(credentials)
+                .WithDefaultSubscription();
+
+            return await azure.VirtualMachines.GetByResourceGroupAsync(resourceGroup, vmName);
+        }
+        public Task StartVirtualMachineAsync(IVirtualMachine vm)
+        {
+            return vm.StartAsync();
+        }
+
+        public Task StopVirtualMachineAsync(IVirtualMachine vm)
+        {
+            return vm.DeallocateAsync();
+        }
+    }
+}
