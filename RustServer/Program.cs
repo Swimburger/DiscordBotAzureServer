af0978841ca62cff12eff045b7c9b60338be71d9
Niels Swimberghe <nielsswimberghe@gmail.com>
2019-04-01 20:54:30 -0400

    add role creation and role checks

--- RustServer/Program.cs
+++ RustServer/Program.cs
@@ -1,16 +1,19 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using Microsoft.Azure.Management.Compute.Fluent;
 using Microsoft.Azure.Management.Compute.Fluent.Models;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RustServer
 {
     public class Program
     {
+        private const string RustDiscordAdminRole = "rust-admin";
         private static DiscordClient discord;
         private static VirtualMachineService virtualMachineService;
         private static readonly Random random = new Random();
@@ -38,51 +41,32 @@ namespace RustServer
         {
             try
             {
-                if (e.Message.Content.ToLower().StartsWith("#!server start", StringComparison.InvariantCultureIgnoreCase))
+                if (e.Author.IsBot)
                 {
-                    var azure = virtualMachineService.GetAzure();
-                    IVirtualMachine vm = await virtualMachineService.GetVirtualMachineAsync(azure);
-                    if (vm.PowerState == PowerState.Running)
-                    {
-                        await e.Message.RespondAsync("Rust server is already running! It takes a few minutes before rust server is connectable.");
-                        return;
-                    }
+                    return;
+                }
 
-                    if (vm.PowerState == PowerState.Starting)
-                    {
-                        await e.Message.RespondAsync("Rust server is already starting!");
-                        return;
-                    }
+                if (e.Guild == null)
+                {
+                    await e.Message.RespondAsync("Bot not allowed outside of Guild");
+                    return;
+                }
 
-                    Task startTask = virtualMachineService.StartVirtualMachineAsync(vm);
-                    await e.Message.RespondAsync("Starting Rust Server!");
-                    await startTask;
-                    await vm.RunPowerShellScriptAsync(new List<string> { "start \"c:\\RustServer\\Start.bat\" /REALTIME" }, new List<RunCommandInputParameter>());
-                    await e.Message.RespondAsync("Rust Server started, ETA 5 min until you can connect! Let's get schwifty");
-                    await e.Message.RespondAsync("Press f1 and type 'client.connect rust.swimburger.net:28015'");
+                if (e.Message.Content.StartsWith("#!server init discord roles", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await InitDiscordRustRole(e);
+                    return;
+                }
+
+                if (e.Message.Content.StartsWith("#!server start", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await StartServer(e);
                     return;
                 }
 
                 if (e.Message.Content.StartsWith("#!server stop", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    var azure = virtualMachineService.GetAzure();
-                    IVirtualMachine vm = await virtualMachineService.GetVirtualMachineAsync(azure);
-                    if (vm.PowerState == PowerState.Deallocated || vm.PowerState == PowerState.Deallocating || vm.PowerState == PowerState.Stopped)
-                    {
-                        await e.Message.RespondAsync("Rust server is stopped!");
-                        return;
-                    }
-
-                    if (vm.PowerState == PowerState.Stopping)
-                    {
-                        await e.Message.RespondAsync("Rust server is already stopping!");
-                        return;
-                    }
-
-                    Task stopTask = virtualMachineService.StopVirtualMachineAsync(vm);
-                    await e.Message.RespondAsync("Stopping Rust Server!");
-                    await stopTask;
-                    await e.Message.RespondAsync("Rust Server stopped, goodnight :(");
+                    await StopServer(e);
                     return;
                 }
 
@@ -97,5 +81,75 @@ namespace RustServer
                 await e.Message.RespondAsync(ex.Message);
             }
         }
+
+        private static async Task InitDiscordRustRole(MessageCreateEventArgs e)
+        {
+            var role = e.Guild.Roles.SingleOrDefault(r => r.Name == RustDiscordAdminRole);
+            if (role == null)
+            {
+                await e.Guild.CreateRoleAsync(RustDiscordAdminRole, reason: "Role for managing rust server via Discord");
+                await e.Message.RespondAsync($"Role created: {RustDiscordAdminRole}");
+            }
+        }
+
+        private static async Task StartServer(MessageCreateEventArgs e)
+        {
+            var member = e.Author as DiscordMember;
+            if(!member.Roles.Any(r => r.Name == RustDiscordAdminRole))
+            {
+                await e.Message.RespondAsync($"{member.DisplayName} does not have '{RustDiscordAdminRole}' role and is not allowed to perform this action.");
+                return;
+            }
+
+            var azure = virtualMachineService.GetAzure();
+            var vm = await virtualMachineService.GetVirtualMachineAsync(azure);
+            if (vm.PowerState == PowerState.Running)
+            {
+                await e.Message.RespondAsync("Rust server is already running! It takes a few minutes before rust server is connectable.");
+                return;
+            }
+
+            if (vm.PowerState == PowerState.Starting)
+            {
+                await e.Message.RespondAsync("Rust server is already starting!");
+                return;
+            }
+
+            var startTask = virtualMachineService.StartVirtualMachineAsync(vm);
+            await e.Message.RespondAsync("Starting Rust Server!");
+            await startTask;
+            await vm.RunPowerShellScriptAsync(new List<string> { "start \"c:\\RustServer\\Start.bat\" /REALTIME" }, new List<RunCommandInputParameter>());
+            await e.Message.RespondAsync("Rust Server started, ETA 5 min until you can connect! Let's get schwifty");
+            await e.Message.RespondAsync("Press f1 and type 'client.connect rust.swimburger.net:28015'");
+        }
+
+        private async static Task StopServer(MessageCreateEventArgs e)
+        {
+            var member = e.Author as DiscordMember;
+            if (!member.Roles.Any(r => r.Name == RustDiscordAdminRole))
+            {
+                await e.Message.RespondAsync($"{member.DisplayName} does not have '{RustDiscordAdminRole}' role and is not allowed to perform this action.");
+                return;
+            }
+
+            var azure = virtualMachineService.GetAzure();
+            var vm = await virtualMachineService.GetVirtualMachineAsync(azure);
+            if (vm.PowerState == PowerState.Deallocated || vm.PowerState == PowerState.Deallocating || vm.PowerState == PowerState.Stopped)
+            {
+                await e.Message.RespondAsync("Rust server is stopped!");
+                return;
+            }
+
+            if (vm.PowerState == PowerState.Stopping)
+            {
+                await e.Message.RespondAsync("Rust server is already stopping!");
+                return;
+            }
+
+            var stopTask = virtualMachineService.StopVirtualMachineAsync(vm);
+            await e.Message.RespondAsync("Stopping Rust Server!");
+            await stopTask;
+            await e.Message.RespondAsync("Rust Server stopped, goodnight :(");
+        }        
     }
 }
